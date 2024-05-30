78fcdeabca9f1af1a336fb0ab7a2bbbc09879695
Mayank Sandilya <78838306+beingkamikaze@users.noreply.github.com>
2024-05-31 00:54:55 +0530

    first commit

---
+++ job-portal-client/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Banner from '../component/Banner'
+
+const Home = () => {
+    const [query,setQuery] = useState("");
+    const handleInputChange = (event) =>{
+        setQuery(event.target.value)
+    }
+    console.log(query)
+    return (
+        <div>
+            <Banner query={query} handleInputChange={handleInputChange}/>
+        </div>
+    )
+}
+
+export default Home
